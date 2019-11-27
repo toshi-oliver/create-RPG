842653cd28844331c314d14980e3b38d53764652
toshi-oliver <igpoid@gmail.com>
2019-11-28 02:58:30 +0900

    add the special_attack method

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ class Brave
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
@@ -10,12 +12,26 @@ class Brave
   end
 
   def attack(monster)
-    damage = @offense - monster.defense
+    puts "#{@name}の攻撃"
+
+    attack_num = rand(4)
+
+    if attack_num == 0
+      puts "必殺攻撃"
+      damage = calculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
     monster.hp -= damage
 
-    puts "#{@name}の攻撃"
     puts "#{monster.name}は#{damage}のダメージを受けた"
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
   end
 
 end
