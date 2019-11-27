0955c145fa5f85c4d9763ce102cf7026822fb5b7
toshi-oliver <igpoid@gmail.com>
2019-11-28 01:49:26 +0900

    define the monster class

--- main.rb
+++ main.rb
@@ -11,7 +11,21 @@ class Brave
 
 end
 
+class Monster
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+  
+end
+
 brave = Brave.new(name:"トシー", hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 puts <<~TEXT
 NAME：#{brave.name}
