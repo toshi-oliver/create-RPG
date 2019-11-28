2713e7db0ee9fa391d9b3caef68a4d3b6567a7ac
toshi-oliver <igpoid@gmail.com>
2019-11-28 18:03:40 +0900

    impliment attack process

--- main.rb
+++ main.rb
@@ -17,11 +17,11 @@ class Brave
     attack_num = rand(4)
 
     if attack_num == 0
-      puts "必殺攻撃"
-      damage = calculate_special_attack - monster.defense
+     puts "必殺攻撃"
+    damage = calculate_special_attack - monster.defense
     else
-      puts "通常攻撃"
-      damage = @offense - monster.defense
+     puts "通常攻撃"
+    damage = @offense - monster.defense
     end
 
     monster.hp -= damage
@@ -37,22 +37,57 @@ class Brave
 end
 
 class Monster
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  attr_reader :offense, :defense
+  attr_accessor :hp, :name
+
+  POWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
 
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
   end
 
-  def attack
+  def attack(brave)
+
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts <<~EOS
+    #{@name}の攻撃
+    #{brave.name}は#{damage}を受けた
+    #{brave.name}の残りHPは#{brave.hp}だ
+    EOS
+
+  end
+
+  private
+
+  def transform
+    transform_name = "ドラゴン"
+
+    puts <<~EOS
+    #{@name}は怒っている
+    #{@name}はドラゴンに変身した
+    EOS
+
+    @name = transform_name
+    @offense *= POWER_UP_RATE
   end
 
 end
 
-brave = Brave.new(name: "トシー", hp: 500, offense: 150, defense: 100)
+brave = Brave.new(name: "トシー", hp: 500, offense: 250, defense: 100)
 monster = Monster.new(name: "ズライム", hp: 250, offense: 200, defense: 100)
 
 brave.attack(monster)
+monster.attack(brave)
