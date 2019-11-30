3f3c7f766bfc805d58bb7071852e8490d3b25a39
toshi-oliver <igpoid@gmail.com>
2019-11-30 17:34:47 +0900

    refactoring GameController.rb

--- games_controller.rb
+++ games_controller.rb
@@ -5,44 +5,50 @@ class GamesController
 
   # バトルの処理
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+    build_characters(params)
 
     loop do
-      brave.attack(monster)
-      break if buttle_end?(monster)
-      monster.attack(brave)
-      break if buttle_end?(brave)
+      @brave.attack(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
     end
 
-    # 勇者の勝敗によってメッセージを変える
-    if battle_result(brave)
-      result = calculate_of_exp_and_gold(monster)
-      puts "#{brave.name}はたたかいに勝った"
-      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
-    else
-      puts "#{brave.name}はたたかいに負けた"
-      puts "目の前が真っ暗になった"
-    end
+    battle_judgment
   end
 
   # 以下のメソッドはクラス外から呼び出す必要がないのでprivate以下に記述する
   private
 
+    def build_characters(**params)
+      @brave = params[:brave]
+      @monster = params[:monster]
+    end
     # バトル終了の判定
-    def buttle_end?(character)
-      character.hp <= 0
+    def battle_end?
+      @brave.hp <= 0 || @monster.hp <= 0
     end
 
     # 勇者の勝利判定
-    def battle_result(brave)
-      brave.hp > 0
+    def battle_win?
+      @brave.hp > 0
+    end
+
+    def battle_judgment
+      if battle_win?
+        result = calculate_of_exp_and_gold
+        puts "#{@brave.name}はたたかいに勝った"
+        puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
+      else
+        puts "#{@brave.name}はたたかいに負けた"
+        puts "目の前が真っ暗になった"
+      end
     end
 
     # 経験値とゴールドの計算
-    def calculate_of_exp_and_gold(monster)
-      exp = (monster.offense + monster.defense) * EXP_CONSTANT
-      gold = (monster.offense + monster.defense) * GOLD_CONSTANT
+    def calculate_of_exp_and_gold
+      exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+      gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
       result = {exp: exp, gold: gold}
 
       result
